e5243a798b9cf4b991c2a0a58ba2819db29cfe6d
dana <danazyt2016@gmail.com>
2023-09-23 13:44:56 -0700

    Added card and button components in confirmscreen

--- components/AppBotton.js
+++ components/AppBotton.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button, StyleSheet, View } from "react-native";
 
-function AppButton({ title, onPress, color, disabled }) {
+function AppButton({ title, onPress, color, disabled = false }) {
   return (
     <View style={styles.buttonContainer}>
       <Button
--- screens/ConfirmScreen.js
+++ screens/ConfirmScreen.js
@@ -1,15 +1,31 @@
 import React from "react";
-import { Modal, Text, StyleSheet, View } from "react-native";
+import { Modal, Text, StyleSheet, SafeAreaView, View } from "react-native";
+import Card from "../components/Card";
+import AppButton from "../components/AppBotton";
 
 function ConfirmScreen({ modalVisibility, hideModal }) {
   return (
     <Modal visible={modalVisibility}>
-      <View styles={styles.container}></View>
+      <SafeAreaView style={styles.container}>
+        <Card>
+          <View style={styles.buttonContainer}>
+            <AppButton title="Go Back" color="red" />
+            <AppButton title="Continue" color="blue" />
+          </View>
+        </Card>
+      </SafeAreaView>
     </Modal>
   );
 }
 
 const styles = StyleSheet.create({
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    width: "100%",
+    padding: 10,
+  },
   container: {
     flex: 1,
     backgroundColor: "lightsteelblue",
