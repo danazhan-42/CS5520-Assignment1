2039fc8de9690c97bca154a36df5ff1c1625ece8
dana <danazyt2016@gmail.com>
2023-09-24 15:22:40 -0700

    Working on the conditional rendering, added two conditions

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -6,11 +6,17 @@ import IncorrectGuessCard from "../components/IncorrectGuessCard";
 import CorrectGuessScreen from "../components/CorrectGuessScreen";
 
 export default function GameScreen(props) {
+  const [userGuess, setUserGuess] = useState();
   const [randomNumber, setRandomNumber] = useState(generateRandom(10, 20));
   const [guessCount, setGuessCount] = useState(0);
   const [isCorrectGuess, setIsCorrectGuess] = useState(false);
 
+  function generateRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
   function handleGuess(guessedNumber) {
+    setUserGuess(guessedNumber);
     setGuessCount((prevCount) => prevCount + 1);
 
     if (guessedNumber == randomNumber) {
@@ -20,13 +26,23 @@ export default function GameScreen(props) {
     }
   }
 
-  function generateRandom(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  const handleTryAgain = () => {
+    setUserGuess("");
+  };
+
+  const handleNewGame = () => {
+    setUserGuess("");
+  };
+
+  let displayScreen;
+
+  if (!userGuess) {
+    <InitialGuessCard onConfirmGuess={handleGuess}></InitialGuessCard>;
+  } else if (userGuess && !isCorrectGuess) {
+    <IncorrectGuessCard onTryAgain={handleTryAgain}></IncorrectGuessCard>;
+  } else {
+    <CorrectGuessScreen onStartNewGame={handleNewGame}></CorrectGuessScreen>;
   }
 
-  return (
-    <Screen>
-      <CorrectGuessScreen></CorrectGuessScreen>
-    </Screen>
-  );
+  return <Screen></Screen>;
 }
